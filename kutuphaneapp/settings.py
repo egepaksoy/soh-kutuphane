73f4ad4c9f07f65b868a44d40520c7dc3572eb0d
Ege Paksoy <egepaskoy@gmail.com>
2022-04-07 19:25:41 +0300

    heroku app url'i eklendi

--- kutuphaneapp/settings.py
+++ kutuphaneapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-du%&j0lnt&1y+q$1lzn-w$m&-2(9b$c$o%g!n+l2xwv!%858&1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "192.168.1.31", "192.168.1.52", "192.168.1.22", "192.168.1.40", "192.168.1.49"]
+ALLOWED_HOSTS = ["sohkutuphane.herokuapp.com", "127.0.0.1", "192.168.1.31", "192.168.1.52", "192.168.1.22", "192.168.1.40", "192.168.1.49"]
     # TODO: okulun kütüphane bilgisayarının ip adresi local olarak eklenilcek ve bir şifre koruması getirilcek
 
 
